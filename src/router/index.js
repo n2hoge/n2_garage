d3d706cb5b5dfa780e97fc39355d131a11000cfb
Naoki Nishimura <pk.afi.n2@gmail.com>
2021-01-14 22:57:02 +0900

    ページ遷移の微修正

--- src/router/index.js
+++ src/router/index.js
@@ -51,7 +51,7 @@ const router = new VueRouter({
 router.beforeEach(function (to, from, next) {
     setTimeout(() => {
         window.scrollTo(0, 0);
-    }, 100);
+    }, 500);
     next();
 });
 

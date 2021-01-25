3d30206724c32a658a85985ab4c8f1c9772cdd58
Naoki Nishimura <pk.afi.n2@gmail.com>
2021-01-25 21:52:27 +0900

    ポートフォリオ紹介文更新

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueMeta from 'vue-meta'
+
 
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -9,6 +11,7 @@ import '@/assets/scss/custom.scss'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
+Vue.use(VueMeta)
 
 new Vue({
   router,
